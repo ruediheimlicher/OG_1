5a6fc2f6f502cd481fe28428019f3144fb1d520d
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2016-08-15 08:45:51 +0200

    add

--- .dep/SPI_Slave_Strom.o.d
+++ .dep/SPI_Slave_Strom.o.d
@@ -1,10 +1,10 @@
-TWI_Slave.o: TWI_Slave.c \
+SPI_Slave_Strom.o: SPI_Slave_Strom.c \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
  /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/iom8.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/iom328p.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
@@ -14,12 +14,13 @@ TWI_Slave.o: TWI_Slave.c \
  /usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/math.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h \
  /usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h \
  /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h twislave.c \
- /usr/local/CrossPack-AVR-20131216/avr/include/util/twi.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h lcd.c \
- lcd.h /usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h adc.c adc.h
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/string.h lcd.c lcd.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h current.c adc.c \
+ adc.h SPI_slave.c
 
 /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h:
 
@@ -31,7 +32,7 @@ TWI_Slave.o: TWI_Slave.c \
 
 /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/iom8.h:
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/iom328p.h:
 
 /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h:
 
@@ -51,17 +52,15 @@ TWI_Slave.o: TWI_Slave.c \
 
 /usr/local/CrossPack-AVR-20131216/avr/include/math.h:
 
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h:
+
 /usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h:
 
 /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h:
 
 /usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h:
 
-twislave.c:
-
-/usr/local/CrossPack-AVR-20131216/avr/include/util/twi.h:
-
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h:
+/usr/local/CrossPack-AVR-20131216/avr/include/string.h:
 
 lcd.c:
 
@@ -69,6 +68,10 @@ lcd.h:
 
 /usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h:
 
+current.c:
+
 adc.c:
 
 adc.h:
+
+SPI_slave.c:
